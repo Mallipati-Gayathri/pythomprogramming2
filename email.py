e69eea0fef623ac07ac7e09bc330ae0dc6d892f8
Mallipati-Gayathri <84333570+Mallipati-Gayathri@users.noreply.github.com>
2021-05-28 17:29:35 +0530

    Add files via upload

---
+++ email.py
@@ -0,0 +1,11 @@
+userlogin=input()
+userpassword=int(input())
+if(userlogin=="gayathri@hotmail.com"):
+    print("login success")
+    if(userpassword==9290):
+        print("password sucess")
+    else:
+        print("recheck password")
+else:
+    print("login failed")
+            
