90533e43690cdb208e2791e5d95444c775682444
Mallipati-Gayathri <84333570+Mallipati-Gayathri@users.noreply.github.com>
2021-05-29 18:55:05 +0530

    Add files via upload

---
+++ gradingacctomarks.py
@@ -0,0 +1,11 @@
+marks=int(input())
+if marks>=80:
+    print("gradeA")
+elif 60>marks<80:
+    print("gradeB")
+elif 40>marks<60:
+    print("grdae C")
+elif marks<40:
+    print("fail")
+else:
+    print("invalid marks")
